9c762ae89803df2cd8e9b58028b69b75b1c828c5
AbdealiJK <abdealikothari@gmail.com>
2016-05-19 11:11:12 +0530

    Add collection hook to deselect items
    Allow expressions to select tests based on the attributes
    appropriately.
    
    It basically performs an eval() of the expr using the attributes
    from the function. This is similar to how -m and -k works.
    To find the attributes (the `locals` for the eval()):
     - First, check if the attribute is found in the function
     - If not, try to find the class of the function (assuming it
       is a class method).
      - If the class is found, check if the attribute is in the class
      - If the class is not found, return None
     - return None as no attribute was found

--- pytest_attrib.py
+++ pytest_attrib.py
@@ -1,9 +1,82 @@
 # -*- coding: utf-8 -*-
 
+import inspect
+import sys
+
 __version__ = '0.1'
 
 
 def pytest_addoption(parser):
     group = parser.getgroup("general")
-    group._addoption("--attr", dest="attr", action="append", metavar="ATTR",
-                     help="Run only tests that have the attributes ATTR")
+    group._addoption("-a",  action="store", default="",
+                     dest="attrexpr", metavar="ATTREXPR",
+                     help='Only run tests matching given attribute expression.'
+                          '  Example: -a "attr1==val1 and attr2==val2".')
+
+def pytest_collection_modifyitems(items, config):
+    attrexpr = config.option.attrexpr
+
+    if not attrexpr:
+        return
+
+    remaining = []
+    deselected = []
+    for colitem in items:
+        if attrexpr and not match_attr(colitem, attrexpr):
+            deselected.append(colitem)
+        else:
+            remaining.append(colitem)
+
+    if deselected:
+        config.hook.pytest_deselected(items=deselected)
+        items[:] = remaining
+
+def match_attr(item, expr):
+    return eval(expr, {}, AttrMapping(item))
+
+def get_class_that_defined_method(meth):
+    # In python3 there is no concept of unbound methods and a class
+    # method is simply a function.
+    # This is a function that attempts to find the class of the method.
+    # Taken from:
+    # http://stackoverflow.com/questions/3589311/get-defining-class-of-unbound-method-object-in-python-3
+    if sys.version_info[0] == 2:
+        return getattr(meth, 'im_class', None)
+    else:
+        if inspect.ismethod(meth):
+            for cls in inspect.getmro(meth.__self__.__class__):
+               if cls.__dict__.get(meth.__name__) is meth:
+                    return cls
+            meth = meth.__func__ # fallback to __qualname__ parsing
+        if inspect.isfunction(meth):
+            cls = getattr(inspect.getmodule(meth),
+                          meth.__qualname__.split('.<locals>', 1)[0]
+                                           .rsplit('.', 1)[0])
+            if isinstance(cls, type):
+                return cls
+    return None  # Failed to find class
+
+
+class AttrMapping:
+    """
+    Provide a mapping for attributes of an item, where if the attribute does
+    not exist, None is given.
+    """
+    def __init__(self, item):
+        self._item = item
+
+    def __getitem__(self, name):
+        obj = self._item.obj
+        MissingAttr = object()
+
+        objattr = getattr(obj, name, MissingAttr)
+        if objattr != MissingAttr:
+            return objattr
+
+        cls = get_class_that_defined_method(obj)
+        if cls is not None:
+            clsattr = getattr(cls, name, MissingAttr)
+            if clsattr != MissingAttr:
+                return clsattr
+
+        return None
--- test_attrib.py
+++ test_attrib.py
@@ -1,6 +1,77 @@
 import pytest
+import unittest
 
 
 def test_arg(testdir):
     result = testdir.runpytest("--help")
-    result.stdout.fnmatch_lines("*--attr=ATTR*")
+    result.stdout.fnmatch_lines("*-a ATTREXPR*")
+
+def test_config(testdir):
+    config = testdir.parseconfig()
+    assert config.getoption('attrexpr') == ''
+
+    config = testdir.parseconfig('-a', 'attr1')
+    assert config.getoption('attrexpr') == 'attr1'
+
+    config = testdir.parseconfig('-a', 'attr1==val1 and attr2==True')
+    assert config.getoption('attrexpr') == 'attr1==val1 and attr2==True'
+
+    config = testdir.parseconfig('-a', 'attr1==val1 and attr2==True',
+                                 '-k', 'somethingelse')
+    assert config.getoption('attrexpr') == 'attr1==val1 and attr2==True'
+
+@pytest.mark.parametrize("spec", [
+    ("xyz", ("test_one",)),
+    ("xyz and xyz2", ()),
+    ("xyz2", ("test_two",)),
+    ("xyz or xyz2", ("test_one", "test_two"),)
+])
+def test_functions(spec, testdir):
+    testdir.makepyfile("""
+        def test_one():
+            pass
+        test_one.xyz = "xyz"
+
+        def test_two():
+            pass
+        test_two.xyz2 = "xyz2"
+    """)
+    opt, passed_result = spec
+    rec = testdir.inline_run("-a", opt)
+    passed, skipped, fail = rec.listoutcomes()
+    passed = [x.nodeid.split("::")[-1] for x in passed]
+    assert len(passed) == len(passed_result)
+    assert set(passed) == set(passed_result)
+
+@pytest.mark.parametrize("spec", [
+    ("xyz", ("test_one",)),
+    ("xyz and xyz2", ()),
+    ("xyz2", ("test_two",)),
+    ("xyz or xyz2", ("test_one", "test_two")),
+    ("xyz3", ("test_three",)),
+    ("xyz or xyz2 or xyz3", ("test_one", "test_two", "test_three")),
+])
+def test_classes(spec, testdir):
+    testdir.makepyfile("""
+        import unittest
+        class OneTest(unittest.TestCase):
+            def test_one(self):
+                pass
+        OneTest.xyz = "xyz"
+
+        class TwoTest(unittest.TestCase):
+            def test_two(self):
+                pass
+        TwoTest.xyz2 = "xyz2"
+
+        class ThreeTest(unittest.TestCase):
+            def test_three(self):
+                pass
+            test_three.xyz3 = "xyz3"
+    """)
+    opt, passed_result = spec
+    rec = testdir.inline_run("-a", opt)
+    passed, skipped, fail = rec.listoutcomes()
+    passed = [x.nodeid.split("::")[-1] for x in passed]
+    assert len(passed) == len(passed_result)
+    assert set(passed) == set(passed_result)
